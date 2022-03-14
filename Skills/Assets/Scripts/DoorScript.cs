66d030c397d71d563b329998593170b443ef167d
Yunazaki <66821940+Yunazaki@users.noreply.github.com>
2022-03-14 09:20:44 -0600

    Refined Door Script

--- Skills/Assets/Scripts/DoorScript.cs
+++ Skills/Assets/Scripts/DoorScript.cs
@@ -8,24 +8,40 @@ public class DoorScript : MonoBehaviour
     public ItemObject key;
 
     private bool hasKey;
+    private bool onDoor = false;
 
     // Update is called once per frame
     void Update()
     {
 
         CheckKey(key);
+        if (onDoor == true && data.input.Move.Interact.IsPressed())
+        {
+            gameObject.SetActive(false);
+        }
 
     }
 
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnTriggerEnter2D(Collider2D col)
     {
-        if (col.collider.CompareTag("Player"))
+        if (col.CompareTag("Player"))
         {
             Debug.Log("Player");
-            if (hasKey && data.isInteracting)
+            if (col.CompareTag("Player"))
             {
-                Debug.Log("Key");
-                gameObject.SetActive(false);
+                onDoor = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Debug.Log("Player");
+            if (col.CompareTag("Player"))
+            {
+                onDoor = false;
             }
         }
     }
